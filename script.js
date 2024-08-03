92e3f55276c7517e2cf524392c0c4706aa1ba808
Fatih <fatihrbl@gmail.com>
2024-08-03 11:47:36 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const tipInput = document.getElementById("tip");
 const totalSpan = document.getElementById("total");
 
 function calculateTotal() {
-    billValue = billInput.value;
-    tipValue = tipInput.value;
-    totalValue = billValue * (1 + tipValue / 100);
+   const billValue = billInput.value;
+   const tipValue = tipInput.value;
+   const totalValue = billValue * (1 + tipValue / 100);
     totalSpan.innerText = totalValue.toFixed(2);
 }
 
